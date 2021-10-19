ca44b14dd89d42b7bd54bb7e76202b0fb5355833
Saj <87875257+SajHere@users.noreply.github.com>
2021-10-19 11:26:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+import os
+PREFIX = '.'
+INTENTS = discord.Intents.default()
+client = discord.Client(commands_prefix=PREFIX, intents=INTENTS)
+
+
+@client.event
+async def on_ready():
+    print(f'Logged in as: {bot.user.name}')
+    print(f'With ID: {bot.user.id}')
+
+@client.command()
+async def load(ctx, extension):
+  client.load_extension(f'cogs.{extension}')
+
+@client.command()
+async def unload(ctx, extension):
+  client.unload_extension(f'cogs.{extension}')
+
+for filename in os.listdir("./cogs"):
+  if filename.endswith(".py"):
+    client.load_extension(f'cogs.{filename[:-3]}')
+
+client.run('ODk5NzMzMzY2MzI3MzQ1MTgy.YW3D_g.rkzoA31nHEeFgWArayZ66XcKfS0')
